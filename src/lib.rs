bba692ec0ee14f78ed4e70fccb05896960bbf1ad
Aayush Borkar <aayushb14@gmail.com>
2023-07-13 16:58:23 +0530

    Add list_models() function

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,21 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub mod palm;
+pub use palm::PalmClient;
+
+pub fn create_client(api_key: String) -> PalmClient {
+    palm::create_client(api_key)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
+        let my_client = create_client(String::from(""));
+        my_client.list_models().expect("err");
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
--- src/palm.rs
+++ src/palm.rs
@@ -1,4 +1,6 @@
-const ENDPOINT: String = String::from("https://generativelanguage.googleapis.com");
+use std::io::Read;
+
+const ENDPOINT: &str = "https://generativelanguage.googleapis.com";
 
 pub struct PalmClient {
     api_key: String,
@@ -6,18 +8,23 @@ pub struct PalmClient {
 }
 
 impl PalmClient {
-    pub fn list_models(&self) -> Result<()> {
-        let body = reqwest::blocking::get(format!(
+    pub fn list_models(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut res = reqwest::blocking::get(format!(
             "{}/v1beta2/models?key={}",
             self.endpoint, self.api_key
-        ))?
-        .text()?;
+        ))?;
+        let mut body = String::new();
+        res.read_to_string(&mut body)?;
+
+        println!("{}",body);
+
+        Ok(())
     }
 }
 
 pub fn create_client(api_key: String) -> PalmClient {
     PalmClient {
         api_key: api_key,
-        endpoint: ENDPOINT,
+        endpoint: String::from(ENDPOINT),
     }
 }
